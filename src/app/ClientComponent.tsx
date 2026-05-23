815e4718b4fac03708b415e958b57f70081f5cd0
Hai Dang <dagger241004abc@gmail.com>
2026-05-23 11:04:41 +0700

    feat: add ClientComponent and update Home page layout
    - Added a new ClientComponent that logs environment configuration.
    - Updated the Home page to simplify the layout and remove unnecessary elements.
    - Added zod as a dependency in package.json.

---
+++ src/app/ClientComponent.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useEffect } from "react";
+import envConfig from "../../config";
+
+const ClientComponent = () => {
+  useEffect(() => {
+    console.log(envConfig);
+  }, []);
+
+  return <div>Client Component</div>;
+};
+
+export default ClientComponent;
