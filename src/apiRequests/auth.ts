7edeebc1245465f40d6447d0b2907379390b72d9
Hai Dang <dagger241004abc@gmail.com>
2026-06-11 16:18:41 +0700

    feat: add refresh token API endpoint and integrate with auth API requests

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -3,6 +3,8 @@ import {
   LoginBodyType,
   LoginResType,
   LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
 } from "@/schemaValidations/auth.schema";
 
 const authApiRequest = {
@@ -33,6 +35,15 @@ const authApiRequest = {
       // client gọi đến route handler của nextjs nên không cần phải truyền accessToken trong header
       // vì nó đã được set trong cookie và sẽ tự động được gửi kèm theo request
     }),
+
+  SRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>("/auth/refresh-token", body),
+
+  refreshToken: () => {
+    http.post<RefreshTokenResType>("/api/auth/refresh-token", null, {
+      baseUrl: "",
+    });
+  },
 };
 
 export default authApiRequest;
