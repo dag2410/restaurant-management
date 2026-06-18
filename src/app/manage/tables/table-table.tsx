813626ab8cda6e568b77b6a52bb37e2111311e35
Hai Dang <dagger241004abc@gmail.com>
2026-06-18 14:25:16 +0700

    feat: add QR code generation for tables and integrate with table management

--- src/app/manage/tables/table-table.tsx
+++ src/app/manage/tables/table-table.tsx
@@ -43,12 +43,14 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
-import { getVietnameseTableStatus } from "@/lib/utils";
+import { getTableLink, getVietnameseTableStatus } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
 import AutoPagination from "@/components/auto-pagination";
 import { TableListResType } from "@/schemaValidations/table.schema";
 import EditTable from "@/app/manage/tables/edit-table";
 import AddTable from "@/app/manage/tables/add-table";
+import { useTableListQuery } from "@/queries/useTable";
+import QRCodeTable from "@/components/qrcode-table";
 
 type TableItem = TableListResType["data"][0];
 
@@ -89,7 +91,14 @@ export const columns: ColumnDef<TableItem>[] = [
   {
     accessorKey: "token",
     header: "QR Code",
-    cell: ({ row }) => <div>{row.getValue("number")}</div>,
+    cell: ({ row }) => (
+      <div>
+        <QRCodeTable
+          token={row.getValue("token")}
+          tableNumber={row.getValue("number")}
+        />
+      </div>
+    ),
   },
   {
     id: "actions",
@@ -167,7 +176,8 @@ export default function TableTable() {
   // const params = Object.fromEntries(searchParam.entries())
   const [tableIdEdit, setTableIdEdit] = useState<number | undefined>();
   const [tableDelete, setTableDelete] = useState<TableItem | null>(null);
-  const data: any[] = [];
+  const tableListQuery = useTableListQuery();
+  const data = tableListQuery.data?.payload.data ?? [];
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
