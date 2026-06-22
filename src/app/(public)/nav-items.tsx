886e1ebcab238e1f56faad5de41d50fcc6ff9706
Hai Dang <dagger241004abc@gmail.com>
2026-06-22 11:19:28 +0700

    feat: enhance navigation items with role-based visibility and logout functionality

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,50 +1,89 @@
 "use client";
 
 import { useAppContext } from "@/components/app-provider";
+import { Role } from "@/constants/type";
+import { cn, handleErrorApi } from "@/lib/utils";
+import { useLogoutMutation } from "@/queries/useAuth";
+import { RoleType } from "@/types/jwt.types";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-const menuItems = [
+const menuItems: {
+  title: string;
+  href: string;
+  role?: RoleType[];
+  hideWhenLogin?: boolean;
+}[] = [
   {
-    title: "Món ăn",
-    href: "/menu",
+    title: "Trang chủ",
+    href: "/",
   },
   {
-    title: "Đơn hàng",
-    href: "/orders",
-    authRequired: true,
+    title: "Menu",
+    href: "/guest/menu",
+    role: [Role.Guest],
   },
   {
     title: "Đăng nhập",
     href: "/login",
-    authRequired: false,
+    hideWhenLogin: true,
   },
   {
     title: "Quản lý",
     href: "/manage/dashboard",
-    authRequired: true,
+    role: [Role.Owner, Role.Employee],
   },
 ];
 
 export default function NavItems({ className }: { className?: string }) {
-  const { isAuth } = useAppContext();
+  const { role, setRole } = useAppContext();
+  const logoutMutation = useLogoutMutation();
+  const router = useRouter();
 
-  return menuItems.map((item) => {
-    if (
-      (item.authRequired === false && isAuth) ||
-      (item.authRequired === true && !isAuth)
-    ) {
-      return null;
+  const logout = async () => {
+    if (logoutMutation.isPending) return;
+    try {
+      await logoutMutation.mutateAsync();
+      setRole();
+      router.push("/");
+      toast.success("Đăng xuất thành công");
+    } catch (error: any) {
+      handleErrorApi({ error });
     }
+  };
 
-    return (
-      <Link
-        suppressHydrationWarning
-        href={item.href}
-        key={item.href}
-        className={className}
-      >
-        {item.title}
-      </Link>
-    );
-  });
+  return (
+    <>
+      {menuItems.map((item) => {
+        // Trường hợp đăng nhập thì chỉ hiển thị menu đăng nhập
+        const isAuth = item.role && role && item.role.includes(role);
+
+        // Trường hợp menu item có thể hiển thị dù cho đã đăng nhập hay chưa
+        const canShow =
+          (item.role === undefined && !item.hideWhenLogin) ||
+          (!role && item.hideWhenLogin);
+
+        if (isAuth || canShow) {
+          return (
+            <Link
+              suppressHydrationWarning
+              href={item.href}
+              key={item.href}
+              className={className}
+            >
+              {item.title}
+            </Link>
+          );
+        }
+
+        return null;
+      })}
+      {role && (
+        <div className={cn(className, "cursor-pointer")} onClick={logout}>
+          Đăng xuất
+        </div>
+      )}
+    </>
+  );
 }
