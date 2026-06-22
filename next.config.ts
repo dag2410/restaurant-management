5e824f5f8616347d059aa6a19219738aefa42ae5
Hai Dang <dagger241004abc@gmail.com>
2026-06-22 11:41:42 +0700

    feat: copy guest menu UI and integrate order API configurations

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
         port: "4000",
         pathname: "/static/**",
       },
+      {
+        hostname: "via.placeholder.com",
+        pathname: "/**",
+      },
     ],
     dangerouslyAllowLocalIP: true,
   },
